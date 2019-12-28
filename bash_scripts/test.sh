8b8e28059a4cbc1cd78a212f8c8764379c2f81be
mark-eva <45572287+mark-eva@users.noreply.github.com>
2019-12-28 15:49:14 +0000

    uploaded a test bash script
    uploaded a test bash script

---
+++ bash_scripts/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+#Description: This is a simple bash script that will be pulled to a fresh vm machine provisioned by terraform. If this 
+#has been executed then the test has passed
+
+echo "Hello world"
+touch /home/oracle/testfile.txt
